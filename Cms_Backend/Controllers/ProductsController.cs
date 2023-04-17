8d520d483a06085967eca5772a43d71f00ee9cce
Oskar Brinck Krüger <obkr38901@edu.ucl.dk>
2023-04-17 13:38:51 +0200

    initial

---
+++ Cms_Backend/Controllers/ProductsController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Cms_Backend;
+using Cms_Backend.Model;
+using MySqlConnector;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace Cms_Backend.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly MyDbContext _context;
+
+        public ProductsController(IConfiguration configuration, MyDbContext context)
+        {
+            _configuration = configuration;
+            _context = context;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var connectionString = _configuration.GetConnectionString("MySqlConnection");
+            using var connection = new MySqlConnection(connectionString);
+            await connection.OpenAsync();
+
+            var query = "SELECT * FROM `products`";
+            using var command = new MySqlCommand(query, connection);
+
+            var products = new List<Product>();
+            using var reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                var product = new Product
+                {
+                    ProductId = reader.GetGuid("product_id"),
+                    ProductName = reader.GetString("product_name"),
+                    ProductCost = reader.GetDecimal("product_cost")
+                };
+                products.Add(product);
+            }
+
+            return products;
+        }
+
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(Guid id)
+        {
+          if (_context.Users == null)
+          {
+              return NotFound();
+          }
+            var product = await _context.Users.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        // PUT: api/Products/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct(Guid id, Product product)
+        {
+            if (id != product.ProductId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Products
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+          if (_context.Users == null)
+          {
+              return Problem("Entity set 'MyDbContext.Users'  is null.");
+          }
+            _context.Users.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduct", new { id = product.ProductId }, product);
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(Guid id)
+        {
+            if (_context.Users == null)
+            {
+                return NotFound();
+            }
+            var product = await _context.Users.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductExists(Guid id)
+        {
+            return (_context.Users?.Any(e => e.ProductId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Cms_Backend/Model/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Cms_Backend.Model
+{
+    public class Product
+    {
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductCost { get; set; }
+    }
+
+
+}
