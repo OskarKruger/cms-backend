c35673b25562efed06666b9665a698c8006f4a28
OskarKruger <obkr38901@edu.ucl.dk>
2023-04-18 13:34:52 +0200

    Connection working, getProducts working, customerID guid not working

---
+++ Cms_Backend/Model/Order.cs
@@ -0,0 +1,18 @@
+﻿namespace Cms_Backend.Model
+{
+    public class Order
+    {
+        public Guid CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public List<OrderItem> Products { get; set; }
+    }
+
+    public class OrderItem
+    {
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductCost { get; set; }
+        public int Quantity { get; set; }
+    }
+
+}
